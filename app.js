8c8b7b8b8bd19213aee7cce76ed5c64360cb88f5
bhavesh-007 <bhaveshmahawar02@gmail.com>
2021-08-29 20:40:11 +0530

    ham

--- app.js
+++ app.js
@@ -2,6 +2,6 @@ var ham = document.getElementById("hamburger1");
 var navlink=document.getElementById("navlink");
 
 ham.addEventListener('click',() =>{
-    navlink.classList.toggle("open");
+ alert("Hamburger Menu Under Development");
     
 });
