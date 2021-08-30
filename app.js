f23b39288563f240ca27a15ecfe8620445fc4104
bhavesh-007 <bhaveshmahawar02@gmail.com>
2021-08-30 20:57:47 +0530

    master

--- app.js
+++ app.js
@@ -1,7 +1,10 @@
 var ham = document.getElementById("hamburger1");
 var navlink=document.getElementById("navlink");
+var download=document.getElementById("resume");
+var ham2=document.getElementById("hamburger2"); 
 
 ham.addEventListener('click',() =>{
- alert("Hamburger Menu Under Development");
-    
+    navlink.classList.toggle("nav-active");
+    download.hidden=true;
+    alert("Bhavesh Mahawar Welcomes You")
 });
